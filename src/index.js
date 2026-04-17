b7047bf7f0c16599572ddaccdac3a1a9c4d3a988
Ananta Cloud <anantacloud-bot@anantacloud.com>
2026-04-17 01:45:10 +0000

    fixed code issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const fs = require("fs");
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as fs from 'fs'
 
 function parseVersion(tag) {
   const match = tag && tag.match(/v?(\d+)\.(\d+)\.(\d+)/);
